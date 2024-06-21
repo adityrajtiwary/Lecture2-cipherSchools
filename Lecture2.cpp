67a3874855844de340640f6d0ba65ea2d119a2bb
adityrajtiwary <122718170+adityrajtiwary@users.noreply.github.com>
2024-06-22 01:17:27 +0530

    Add files via upload

---
+++ Lecture2.cpp
@@ -0,0 +1,13 @@
+#include<iostream> // This is header part
+
+using namespace std; // Std makes sure that cout is a part of code
+
+int main() // this is main function from where your code actually runs and its also the one who is litreally going to be execute first
+{
+	cout<<"hello world"; // cout is a printing statement use in c++ for printing the text you're providing inside the double quotes 
+	return 0;
+}  
+
+
+
+
